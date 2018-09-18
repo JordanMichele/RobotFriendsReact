f17070464996aae1d49243f2b2590e9a98e4ca75
JordanMichele <jordanmichele32@gmail.com>
2018-09-18 18:13:54 -0400

    added styling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import React, { Component } from 'react';
-import Cardlist from './Cardlist';
-import SearchBox from './SearchBox';
-import { robots } from  './robots';
+import React, { Component } from "react";
+import Cardlist from "./Cardlist";
+import SearchBox from "./SearchBox";
+import { robots } from "./robots";
+import "./App.css";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       robots: robots,
-      searchfield: ''
-    }
+      searchfield: ""
+    };
   }
 
-onSearchChange = (event) => {
-  this.setState({ searchfield: event.target.value })
-}
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+  };
 
   render() {
     const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
     return (
-      <div className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <Cardlist robots={filteredRobots}/>
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Cardlist robots={filteredRobots} />
       </div>
     );
   }
